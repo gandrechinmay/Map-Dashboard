4920655c7483a1967314e47ff6c4e424452f59f5
Chinmay Gandre <154724189+gandrechinmay@users.noreply.github.com>
2024-12-11 00:59:16 +0000

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,6 @@ import pandas as pd
 print("getting data.....")
 df = px.data.carshare()
 
-app.layout = [
-    html.H1(children='Title of Dash App', style={'textAlign':'center'}),
-    dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
-    dcc.Graph(id='graph-content')
-]
-
 fig = px.scatter_map(df,lat='centroid_lat',lon='centroid_lon', color='peak_hour',size='car_hours',color_continuous_scale=px.colors.cyclical.IceFire, size_max=15, zoom=10, title = 'Car Share Data', )
 fig.update_layout(mapbox_style='open-street-map')
 fig.show()
