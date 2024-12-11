0719dec8702193da5d39d22a8c364571d654f2c7
Chinmay Gandre <154724189+gandrechinmay@users.noreply.github.com>
2024-12-11 00:18:22 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+from dash import Dash, html, dcc, callback, Output, Input
+import plotly.express as px
+import pandas as pd
+
+print("getting data.....")
+df = px.data.carshare()
+app = Dash()
+
+app.layout = [
+    html.H1(children='Title of Dash App', style={'textAlign':'center'}),
+    dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
+    dcc.Graph(id='graph-content')
+]
+
+fig = px.scatter_map(df,lat='centroid_lat',lon='centroid_lon', color='peak_hour',size='car_hours',color_continuous_scale=px.colors.cyclical.IceFire, size_max=15, zoom=10, title = 'Car Share Data', )
+fig.update_layout(mapbox_style='open-street-map')
+fig.show()
